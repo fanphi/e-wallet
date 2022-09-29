d91ed8442988393ab3765a849e7ff6130e9ff77b
fanphi <fannyp90@hotmail.com>
2022-09-29 23:13:20 +0200

    added activate card

--- src/components/card/AllCards.jsx
+++ src/components/card/AllCards.jsx
@@ -20,7 +20,7 @@ const handleActivate = () => {
 
 return(
     <>
-    
+
       {cardInfo.activated ? <div className="active-card"> 
              <p className="card-vendor">{cardInfo.vendor}</p>
              <p className="card-number">{cardInfo.cardNumber}</p>
@@ -30,17 +30,15 @@ return(
              </span>
              </div>
              :
-             <div className="card"> 
+             <div className="card" onClick={handleActivate}> 
              <p className="card-vendor">{cardInfo.vendor}</p>
              <p className="card-number">{cardInfo.cardNumber}</p>
              <span className="card-info">
              <p className="card-name"> <span className="name-valid">CARDHOLDER'S NAME</span> {firstName + " " + lastName}</p>
              <p className="card-valid"><span className="name-valid">VALID</span> {cardInfo.expireMonth} / {cardInfo.expireYear} </p>
              </span>
-             {cardInfo.activated === false && 
-             <button onClick={handleDelete}>Delete Card</button>}
-              {!cardInfo.activated && 
-             <button onClick={handleActivate}>Activate Card</button>}
+             
+             <button onClick={handleDelete}>Delete Card</button>
              </div>
               }
 
